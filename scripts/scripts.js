f01a243adee6c1d6aeb2b138d8725f562dcc7b6b
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-09-23 10:41:16 -0700

    Refactor
    Trying a different approach

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,20 +13,9 @@ let v2 = document.querySelector("#v2");
 let clacBtn = document.querySelector("#btn-calc");
 let showOutput = document.querySelector("#showOutput");
 
-let arrayInterest = ['concentration' , 'volume']; 
-
-function showNextStep() {
-
-    console.log(selectVol.value); 
-
-    if (selectVol.value === 'volume') {
-        console.log("you selected " + selectVol.value) 
-    } else if (selectConc.value === 'concentration') {
-        console.log("you selected " + selectConc.value)
-    } else {
-        console.log("Please give a valid input"); 
-    }
-}
 
 
-nextBtn.addEventListener("click", showNextStep);
+
+
+
+
